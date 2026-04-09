b515553142114b4295b33c6316a654ee36bd9f56
Inácio Souza Rocha <inaciosouzarocha69@gmail.com>
2026-04-09 14:52:25 -0300

    refactor: Types e Interfaces que estavam espalhados pela aplicação foram concentrados na pasta types

---
+++ src/components/MovieList/MovieList.tsx
@@ -0,0 +1,21 @@
+import type { Movie } from '../../types';
+import CardMovie from '../CardMovie/CardMovie';
+import styles from './MovieList.module.css'
+
+interface MovieListProps {
+    movies: Movie[];
+}
+
+const MovieList = ({ movies }: MovieListProps) => {
+    return (
+        <ul className={styles.lista}>
+            {
+                movies.map(
+                    (movie) => <CardMovie key={movie.id} {...movie}/>
+                )
+            }
+        </ul>
+    );
+}
+
+export default MovieList;

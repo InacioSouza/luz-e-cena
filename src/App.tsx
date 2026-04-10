aaee076893cd4b5bb6c2f5beca8ab75668c99a14
Inácio Souza Rocha <inaciosouzarocha69@gmail.com>
2026-04-10 08:29:07 -0300

    ui: Componente Newsletter implementado

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Banner from "./components/Banner/Banner"
 import Header from "./components/Header/Header"
 import MovieSection from "./components/MovieSection/MovieSection"
+import Newsletter from "./components/Newsletter/Newsletter"
 
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
       <Header/>
       <Banner src="./images/banner.png" alt="Banner"/>
       <MovieSection/>
+      <Banner src="./images/combo.png" alt="Combo"/>
+      <Newsletter/>
     </>
   )
 }

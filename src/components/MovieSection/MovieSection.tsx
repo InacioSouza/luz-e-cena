a9e30957d91de9eb4b74004e3a891e3a2c47f9f0
Inácio Souza Rocha <inaciosouzarocha69@gmail.com>
2026-04-09 21:40:33 -0300

    feat: Funcionalidade de filtrar filmes implementada

--- src/components/MovieSection/MovieSection.tsx
+++ src/components/MovieSection/MovieSection.tsx
@@ -5,22 +5,34 @@ import InputText from '../InputText/InputText';
 import styles from './MovieSection.module.css'
 import MovieList from '../MovieList/MovieList';
 import useFetchMovies from '../../hooks/useFetchMovies';
+import useFilterMovies from '../../hooks/useFilterMovies';
+import type React from 'react';
 
 const MovieSection = () => {
 
     const { movies, error, isLoading } = useFetchMovies();
+    const {searchTerm, handleSearch, setSearchTerm, filteredMovies} = useFilterMovies(movies);
 
     return (
         <main>
             <section className={styles.container}>
                 <Fieldset variant='secondary'>
-                    <InputText placeholder='Buscar filmes ...'></InputText>
-                    <Button variant='icon'>
+                    <InputText 
+                        value={searchTerm}
+                        onChange={
+                            (event: React.ChangeEvent<HTMLInputElement>) => 
+                                setSearchTerm(event.target.value)
+                        }
+                        placeholder='Buscar filmes ...'>
+                    </InputText>
+                    <Button variant='icon' onClick={handleSearch}>
                         <FaSearch />
                     </Button>
                 </Fieldset>
                 <h1 className={styles.titulo}>Em cartaz</h1>
-                <MovieList movies={movies} />
+                {error && <p className={styles.error}>{error}</p>}
+                {isLoading && <p>Carregando Filmes...</p>}
+                <MovieList movies={filteredMovies} />
             </section>
         </main>
     );
